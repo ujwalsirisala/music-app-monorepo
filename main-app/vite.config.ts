4f01c704b58959b736983d45ed3a4ea806efa223
sirisala ujwal <125348019+ujwalsirisala@users.noreply.github.com>
2025-08-31 12:41:44 +0530

    Add files via upload

---
+++ main-app/vite.config.ts
@@ -0,0 +1,24 @@
+// main-app/vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'mainApp',
+      remotes: {
+        // Point to the running MFE dev server
+        musicLibrary: 'http://localhost:5001/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  }
+});
